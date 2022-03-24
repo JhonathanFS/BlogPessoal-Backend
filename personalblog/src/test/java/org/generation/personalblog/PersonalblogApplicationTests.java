d28afdffde99dfc3fd47f6999d75f88e9efb28db
Jokyeon <jhonathan.fs@hotmail.com>
2022-03-24 20:25:50 -0300

    first commit

---
+++ personalblog/src/test/java/org/generation/personalblog/PersonalblogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.personalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
